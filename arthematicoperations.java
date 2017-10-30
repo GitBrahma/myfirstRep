35c05b47d1eb7a929f38ac757518888339aa8818
GitBrahma <venkat_kothapalli@yahoo.com>
2017-10-30 16:40:26 +0530

    addition, subtraction and multiply methods defined

--- arthematicoperations.java
+++ arthematicoperations.java
@@ -2,12 +2,25 @@ class arthematicoperations{
 
 public void main(String[] args){
 
-add(2,3)
+  int a=add(2,3);
+  int s=sub(2,3);
+  int m=mul(2,3);
+  System.out.println("Additio: "+a+" Subtraction: "+s+" Multiplication: "+m);
+  
 
 }
 public int add(int a, int b){
-
+  return a+b;
 }
+  
+ public int sub(int a, int b){
+  return a-b;
+}
+  
+  public int mul(int a, int b){
+  return a*b;
+}
+
 
 
 }
